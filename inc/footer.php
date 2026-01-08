1a82e660cee3e4f52a587740ecccf162141b7488
trannhon70 <kevintran351996@gmail.com>
2026-01-08 11:57:01 +0700

    update

--- inc/footer.php
+++ inc/footer.php
@@ -1,217 +1,64 @@
  <footer class="footer" id="footer_pc">
-     <div class="footer__support">
-         <div class="footer__support-body">
-             <h5>BẠN CẦN HỖ TRỢ?</h5>
-             <span>LIÊN HỆ NGAY VỚI CHÚNG TÔI ĐỂ ĐƯỢC TƯ VẤN</span>
-             <div class="footer__support-body-icon">
-                 <img loading="lazy" class="footer__support-body-icon-img"
-                     src="<?php echo $local ?>/images/icons/icon_dash.webp" height="13px" width="750px" alt="..."></img>
-             </div>
-             <div class="footer__support-body-row">
-                 <a href="<?php echo $local ?>" class="footer__support-body-card">
-                     <img loading="lazy" class="footer__support-body-card-img"
-                         src="<?php echo $local ?>/images/icons/icon_group_user.webp" height="54px" width="80px"
-                         alt="..."></img>
-                     <div class="footer__support-body-text">
-                         <h5>028-7776-7777</h5>
-                         <div>Tổng đài hỗ trợ khách hàng 24/7</div>
-                     </div>
-                 </a>
-                 <a href="<?php echo $local ?>" class="footer__support-body-card">
-                     <img loading="lazy" class="footer__support-body-card-img"
-                         src="<?php echo $local ?>/images/icons/icon_group_chat.webp" height="54px" width="80px"
-                         alt="..."></img>
-                     <div class="footer__support-body-text">
-                         <h5>Chat với nhân viên</h5>
-                         <div>Giải đáp thắc mắc về quy trình</div>
-                     </div>
-                 </a>
-             </div>
-         </div>
-     </div>
-     <div class="footer__contact">
-         <div class="footer__contact-left">
-             <img loading="lazy" class="footer__contact-right-img" src="<?php echo $local ?>/images/logo/logo2.webp"
-                 height="72px" width="300px" alt="..."></img>
-             <div class="footer__contact-left-title">
-                 <span>GIỜ LÀM VIỆC</span>
-             </div>
-             <div class="footer__contact-left-now">
-                 8:00 - 20:00
-             </div>
-             <div class="">
-                 Tất cả các ngày trong tuần, kể cả ngày lễ
-             </div>
-             <div class="footer__contact-left-title">
-                 <span>THÔNG TIN LIÊN HỆ</span>
-             </div>
-             <div class="footer__contact-left-card">
-                 <img loading="lazy" class="footer__contact-left-card-img"
-                     src="<?php echo $local ?>/images/icons/icon_phone_no.webp" height="34px" width="36px"
-                     alt="..."></img>
-                 <div class="footer__contact-left-card-div">
-                     <h5>HOTLINE</h5>
-                     <span>028-7776-7777</span>
-                 </div>
-             </div>
-             <div class="footer__contact-left-card">
-                 <img loading="lazy" class="footer__contact-left-card-img"
-                     src="<?php echo $local ?>/images/icons/icon_location.webp" height="34px" width="36px"
-                     alt="..."></img>
-                 <div class="footer__contact-left-card-div">
-                     <h5>ĐỊA CHỈ</h5>
-                     <span>73 Kinh Dương Vương, P.12, Q.6, TP.HCM</span>
-                 </div>
-             </div>
-             <div class="footer__contact-left-card">
-                 <img loading="lazy" class="footer__contact-left-card-img"
-                     src="<?php echo $local ?>/images/icons/icon_mail.webp" height="34px" width="36px" alt="..."></img>
-                 <div class="footer__contact-left-card-div">
-                     <h5>MAIL</h5>
-                     <span>pknhatviet@gmail.com</span>
-                 </div>
-             </div>
-         </div>
-         <div class="footer__contact-right">
-             <img loading="lazy" class="footer__contact-right-img" src="<?php echo $local ?>/images/banner/Map.webp"
-                 height="390px" width="680px" alt="..."></img>
-         </div>
-     </div>
-     <div class="footer__copyRight">
-         <div>Copyright © <?php echo date('Y'); ?> JV Nhật Việt</div>
-     </div>
+
  </footer>
 
- <footer class="footer__mobile" id="footer__mobile">
-     <div>
-         <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/banner/footer_mobile.webp"
-             alt="...">
-     </div>
-     <div style="margin-top: 5px;">
-         <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/banner/Map.webp" alt="...">
-     </div>
-     <div class="footer__mobile-body">
-         <div class="footer__mobile-container">
-             <a href="tel:+02877767777" class="footer__mobile-container-left">
-                 <div class="footer__mobile-container-left-top">TƯ VẤN TRỰC TUYẾN 24/7</div>
-                 <span class="footer__mobile-container-left-bottom">028-7776-7777</span>
-                 <div class="footer__mobile-container-left-icon">
-                     <img loading="lazy" width="65px" height="auto"
-                         src="<?php echo $local ?>/images/logo_mobile/icon_phone_red.png" alt="...">
-                 </div>
-                 <div class="footer__mobile-container-left-icon-top">
-                     <span>GỌI NGAY</span>
-                 </div>
-             </a>
-             <a href="<?php echo $local ?>" class="footer__mobile-container-right">
-                 <div>ĐẶT LỊCH</div>
-                 <img loading="lazy" width="70px" height="auto" src="<?php echo $local ?>/images/icons/icon_lich.png"
-                     alt="...">
-             </a>
-         </div>
-     </div>
- </footer>
- <div class="footer_list_icon">
-     <div>
-         <a class="footer_icon_happy" href="<?php echo $local ?>" target="_blank">
-             <img loading="lazy" src="<?php echo $local ?>/images/logo_mobile/icon_happy.gif" height="50px" width="50px"
-                 alt="..."></img>
-         </a>
-     </div>
-     <div style="margin-top:10px">
-         <a class="footer_icon_zalo" href="<?php echo $local ?>" target="_blank">
-             <img loading="lazy" src="<?php echo $local ?>/images/logo_mobile/icon_zalo.png" height="50px" width="50px"
-                 alt="..."></img>
-             <div class="ping_zalo"></div>
-         </a>
-     </div>
-     <div style="margin-top:10px">
-         <a class="footer_icon_mess" href="<?php echo $local ?>" target="_blank">
-             <img loading="lazy" style="margin-left:3px" src="<?php echo $local ?>/images/logo_mobile/icon_message.webp"
-                 height="45px" width="45px" alt="..."></img>
-             <div class="ping"></div>
-             <div class="footer_list_icon_number">10</div>
-         </a>
-     </div>
- </div>
- <?php include "layout/modelThongBao.php" ?>
 
 
- <script async src="<?php echo $local ?>/js/random_number.min.js"></script>
 
- <script defer>
-     function toastr(message, type = 'info') {
-         const toastContainer = document.getElementById('toast-container');
-         if (!toastContainer) return;
-
-         // Create toast element
-         const toast = document.createElement('div');
-         toast.classList.add('toast', type);
-         toast.innerHTML = ` ${message}`;
-
-         // Append toast to container
-         toastContainer.appendChild(toast);
-
-         // Remove toast after 3 seconds
-         setTimeout(() => {
-             toast.remove();
-         }, 3000);
-     }
- </script>
  <script>
-     document.addEventListener('DOMContentLoaded', () => {
-         function updateHeaderScripts() {
-             // Xóa các script cũ nếu có
-             const existingMobileScripts = document.querySelectorAll('script[id^="mobile-"]');
-             const existingDesktopScripts = document.querySelectorAll('script[id^="desktop-"]');
-             existingMobileScripts.forEach(script => script.remove());
-             existingDesktopScripts.forEach(script => script.remove());
+document.addEventListener('DOMContentLoaded', () => {
+    function updateHeaderScripts() {
+        // Xóa các script cũ nếu có
+        const existingMobileScripts = document.querySelectorAll('script[id^="mobile-"]');
+        const existingDesktopScripts = document.querySelectorAll('script[id^="desktop-"]');
+        existingMobileScripts.forEach(script => script.remove());
+        existingDesktopScripts.forEach(script => script.remove());
 
-             if (window.innerWidth < 1000) {
-                 const mobileScripts = [{
-                         src: '<?php echo $local ?>/js/mobile.min.js',
-                         id: 'mobile-0'
-                     },
-                     // {
-                     //     src: 'js/siderbar_mobile.min.js',
-                     //     id: 'mobile-1'
-                     // },
+        if (window.innerWidth < 1000) {
+            const mobileScripts = [{
+                    src: '<?php echo $local ?>/js/mobile.min.js',
+                    id: 'mobile-0'
+                },
+                // {
+                //     src: 'js/siderbar_mobile.min.js',
+                //     id: 'mobile-1'
+                // },
 
-                 ];
-                 mobileScripts.forEach(({
-                     src,
-                     id
-                 }) => {
-                     const script = document.createElement('script');
-                     script.src = src;
-                     script.id = id;
-                     document.body.appendChild(script);
-                 });
-             } else {
-                 const desktopScripts = [{
-                         src: '<?php echo $local ?>/js/slider.min.js',
-                         id: 'desktop-0'
-                     },
+            ];
+            mobileScripts.forEach(({
+                src,
+                id
+            }) => {
+                const script = document.createElement('script');
+                script.src = src;
+                script.id = id;
+                document.body.appendChild(script);
+            });
+        } else {
+            const desktopScripts = [{
+                    src: '<?php echo $local ?>/js/slider.min.js',
+                    id: 'desktop-0'
+                },
 
-                 ];
-                 desktopScripts.forEach(({
-                     src,
-                     id
-                 }) => {
-                     const script = document.createElement('script');
-                     script.src = src;
-                     script.id = id;
-                     document.body.appendChild(script);
-                 });
-             }
-         }
+            ];
+            desktopScripts.forEach(({
+                src,
+                id
+            }) => {
+                const script = document.createElement('script');
+                script.src = src;
+                script.id = id;
+                document.body.appendChild(script);
+            });
+        }
+    }
 
-         updateHeaderScripts();
+    updateHeaderScripts();
 
-         window.addEventListener('resize', () => {
-             updateHeaderScripts();
-         });
-     });
+    window.addEventListener('resize', () => {
+        updateHeaderScripts();
+    });
+});
  </script>
  </body>
 
